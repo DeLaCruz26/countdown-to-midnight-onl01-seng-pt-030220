a47b0be11afd6e9b982b2a0b8e086bc90ebe11de
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-08 07:35:28 +0000

    Automatically backed up by Learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,8 +1,9 @@
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
+def countdown_with_sleep(5)
+  x = 0
+  while x > 10
+    puts "#{x} SECOND(S)!"
+    sleep(5.seconds)
+    x -= 1
   end
-  "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
